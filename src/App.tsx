3f5436334cd4540fc2370be58d3a47384552b1cc
Revaz Sologhashvili <rsologhashvili@evex.ge>
2024-06-21 16:48:01 +0400

    add updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,7 @@ function App() {
           <h1 className="heading-main">Site Under Construction</h1>
         </div>
         <div className="imageContainer">
-          <img
-            className="img"
-            src="/gc-logo.png"
-            alt="Georgian Clinics"
-            width="auto"
-            height="auto"
-          />
+          <img className="img" src="/gc-logo.png" alt="Georgian Clinics" />
         </div>
       </section>
     </main>
