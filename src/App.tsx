11874f0eda3c2af5eb1415aa7f586549fa6c9103
Revaz Sologhashvili <rsologhashvili@evex.ge>
2024-06-21 16:12:48 +0400

    upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <main>
+      <section id="UnderConstruction">
+        <div className="main">
+          <h1 className="heading-main">Site Under Construction</h1>
+        </div>
+        <div className="imageContainer">
+          <img
+            className="img"
+            src="/gc-logo.png"
+            alt="Georgian Clinics"
+            width="auto"
+            height="auto"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
